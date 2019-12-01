0eff8361cc8d1bab64d672511e2c9b864de1b2a9
Hirsh Ramani <hrcomazing@comcast.net>
2019-12-01 16:53:25 -0500

    initial add of RoboPosTune.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboPosTune.java
@@ -0,0 +1,160 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class RoboPosTune extends LinearOpMode {
+    BNO055IMU imu;
+    ExtraClasses extraClasses;
+    Orientation angles;
+    DistanceSensor rangeSensorLeft;
+    DistanceSensor rangeSensorBack;
+    DcMotorEx leftMotor;
+    DcMotorEx leftMotor2;
+    DcMotorEx rightMotor;
+    DcMotorEx rightMotor2;
+    DcMotorEx middleMotor;
+    DcMotorEx leftIntakeMotor;
+    DcMotorEx rightIntakeMotor;
+    DcMotorEx arm;
+    Servo clawServo;
+    double angleDouble = 0;
+    double mid = 58;
+    double back = 24;
+    private boolean newLeftTriggerPressed = true;
+    private boolean newRightTriggerPressed = true;
+    private boolean newLeftBumperPressed = true;
+    private boolean newRightBumperPressed = true;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        leftMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "Left Motor Front");
+        leftMotor2 = (DcMotorEx) hardwareMap.get(DcMotor.class, "Left Motor Back");
+        rightMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "Right Motor Front");
+        rightMotor2 = (DcMotorEx) hardwareMap.get(DcMotor.class, "Right Motor Back");
+        middleMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "Middle Motor");
+        rightIntakeMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "Intake Motor Right");
+        leftIntakeMotor = (DcMotorEx) hardwareMap.get(DcMotor.class, "Intake Motor Left");
+        arm = (DcMotorEx) hardwareMap.get(DcMotor.class, "Arm");
+        clawServo = hardwareMap.get(Servo.class, "Claw Servo");
+        waitForStart();
+        while (opModeIsActive()) {
+
+            angles = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+            angleDouble = Double.parseDouble(ExcessStuff.formatAngle(angles.angleUnit, angles.firstAngle));
+            if (gamepad1.left_trigger != 0 && newLeftTriggerPressed){
+                back --;
+                newLeftTriggerPressed = false;
+
+            }else if (gamepad1.right_trigger !=0  && newRightTriggerPressed){
+                back ++;
+                newRightTriggerPressed = false;
+            }
+            if (gamepad1.left_bumper && newLeftBumperPressed){
+                mid --;
+                newLeftBumperPressed = false;
+
+            }else if (gamepad1.right_bumper && newRightBumperPressed){
+                mid ++;
+                newRightBumperPressed = false;
+            }
+            if (gamepad1.left_trigger == 0){
+                newLeftTriggerPressed = true;
+            }
+            if (gamepad1.right_trigger == 0){
+                newRightTriggerPressed = true;
+            }
+            if (!gamepad1.left_bumper){
+                newLeftBumperPressed = true;
+            }
+            if (!gamepad1.right_bumper){
+                newRightBumperPressed = true;
+            }
+            autoScore(mid, back);
+
+        }
+    }
+    public void autoScore(double mid, double back) {
+        double goalAngle = 0;
+        double distanceDifferenceMid = 100;
+        double distanceDifferenceBack = 100;
+        if(!extraClasses.closeEnough(angleDouble, goalAngle, 3) || Math.abs(distanceDifferenceMid) > 2 || Math.abs(distanceDifferenceBack) > 2) {
+
+            goalAngle = 0; //Just the starting angle I think
+            double distance1 = Math.abs(angleDouble - goalAngle);
+            double distance2 = Math.abs(Math.abs((360 - angleDouble)) - goalAngle);
+            double angleError = distance1;
+            double angleAdjustPower = 0;
+            if (distance1 > distance2) {
+                angleError = distance2;
+            }
+            if ((goalAngle - angleDouble + 360) % 360 < 180) {
+                angleError = angleError * -1;
+            } else {
+                angleError = angleError;
+            }
+            angleAdjustPower = angleError / 60;
+
+            //Find how far from the side wall it is
+            double setDistanceItShoudldBeBack = back;
+            double rangeSensorDistanceBack = rangeSensorBack.getDistance(DistanceUnit.CM);
+            distanceDifferenceBack = rangeSensorDistanceBack - setDistanceItShoudldBeBack;
+            double frontPowerError = distanceDifferenceBack / 50;
+
+            //Find how far from the foundation it is
+            double setDistanceItShoudldBeMid = mid;
+            double rangeSensorDistanceMid = rangeSensorLeft.getDistance(DistanceUnit.CM);
+            double rangeSensorValueUsed = rangeSensorDistanceMid;
+            distanceDifferenceMid = rangeSensorValueUsed - setDistanceItShoudldBeMid;
+            double middlePowerError = distanceDifferenceMid / 40;
+
+            leftMotor.setPower(-frontPowerError + angleAdjustPower);
+            leftMotor2.setPower(-frontPowerError + angleAdjustPower);
+            rightMotor.setPower(-frontPowerError + -angleAdjustPower);
+            rightMotor2.setPower(-frontPowerError + -angleAdjustPower);
+            middleMotor.setPower(middlePowerError);
+        }
+        leftMotor.setPower(0);
+        leftMotor2.setPower(0);
+        rightMotor.setPower(0);
+        rightMotor2.setPower(0);
+        middleMotor.setPower(0);
+        arm.setTargetPosition(-3500);
+        arm.setPower(.5);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        if(arm.isBusy()) {
+
+        }
+        clawServo.setPosition(.68);
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        arm.setTargetPosition(-100);
+        arm.setPower(.5);
+
+    }
+}
